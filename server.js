2a8c160e466462167df514217538c436eba2c864
shadrach68 <ugwokeshadrach68@gmail.com>
2026-02-01 12:35:30 +0100

    setup server

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ import axios from "axios"
 
 const app = express()
 const port = 3000
+app.use(express.static("public"));
+
+app.get("/", (req, res)=>{
+    res.render("index.ejs")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+});
